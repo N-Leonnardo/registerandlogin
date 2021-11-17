81f05216db3d097c6a2865c7e4dd1eb014bc968c
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2021-11-16 22:40:33 -0800

    innitial commit

--- server.py
+++ server.py
@@ -1,26 +1,71 @@
-from flask import Flask, render_template, request, redirect
-# import the class from friend.py
-from user import Email
+from flask import Flask, render_template, request, redirect, session, flash
+from user import User
 
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
 
-@app.route('/create_email', methods=["POST"])
-def create_email():
-    if not Email.validate_user(request.form):
-        # we redirect to the template with the form.
-        return redirect('/')
-    data = {
-        "email" : request.form["email"]
-    }
-    Email.save(data)
-    return redirect('/emails')
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
-@app.route("/emails")
-def showemails():
-    emails = Email.get_all()
-    print(emails)
-    return render_template("newuser.html", emails=emails)
+@app.route('/register/user', methods=['POST'])
+def register():
+    # validate the form here ...
+    if not User.validate_user(request.form):
+        return redirect('/')
+    # create the hash
+    pw_hash = bcrypt.generate_password_hash(request.form['pw'])
+    print(pw_hash)
+    # put the pw_hash into the data dictionary
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "pw" : pw_hash
+    }
+    # Call the save @classmethod on User
+    user_id = User.save(data)
+    # store user id into session
+    session['user_id'] = user_id
+    return redirect("/dashboard")
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # see if the username provided exists in the database
+    data = { "email" : request.form["email"] }
+    user_in_db = User.get_by_email(data)
+    # user is not registered in the db
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.pw, request.form['pw']):
+        # if we get False after checking the password
+        flash("Invalid Email/Password")
+        return redirect('/')
+    # if the passwords matched, we set the user_id into session
+    session['user_id'] = user_in_db.id
+    # never render on a post!!!
+    return redirect("/dashboard")
+
+@app.route('/dashboard')
+def dashboard():
+    data = {
+        "user_id": session['user_id']
+    }
+    users = User.get_byid(data)
+    return render_template('dashboard.html', users=users)
+
+@app.route("/users")
+def showusers():
+    users = User.get_all()
+    print(users)
+    return render_template("newuser.html", users=users)
+
+@app.route("/logout", methods=['POST'])
+def clearsession():
+    session.clear()
+    return redirect("/")
 
 
 @app.route("/")
--- user.py
+++ user.py
@@ -2,12 +2,17 @@ from mysqlconnection import connectToMySQL
 import re
 from flask import flash
 
+
+
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
-class Email:
+class User:
     def __init__( self , data ):
         self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
         self.email = data['email']
+        self.pw = data['pw']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
@@ -20,17 +25,31 @@ class Email:
         return is_valid
 
     @classmethod
+    def get_byid(cls, data:dict):
+        query = "SELECT * FROM users WHERE id = %(user_id)s;"
+        results = connectToMySQL('users').query_db(query, data)
+        return cls (results[0])
+
+    @classmethod
     def get_all(cls):
-        query = "SELECT * FROM emails;"
-        results = connectToMySQL('emailchecker').query_db(query)
-        emails = []
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('users').query_db(query)
+        users = []
         for email in results:
-            emails.append( cls(email) )
-        return emails
+            users.append( cls(email) )
+        return users
     
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL("users").query_db(query,data)
+        # Didn't find a matching user
+        if len(result) < 1:
+            return False
+        return cls(result[0])
 
     @classmethod
     def save(cls, data ):
-        query = "INSERT INTO emails ( email , created_at, updated_at ) VALUES ( %(email)s , NOW() , NOW() );"
+        query = "INSERT INTO users ( first_name, last_name, email, pw, created_at, updated_at ) VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(pw)s , NOW() , NOW() );"
 
-        return connectToMySQL('emailchecker').query_db( query, data )
+        return connectToMySQL('users').query_db( query, data )
